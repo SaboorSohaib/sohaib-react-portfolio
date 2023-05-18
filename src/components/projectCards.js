95cab40e7baa591b152c801d4ca6a4e55651c8aa
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-05-18 06:14:14 +0430

    ADD SOME CARDS

--- src/components/projectCards.js
+++ src/components/projectCards.js
@@ -1,19 +1,83 @@
 import React from "react";
+import worldVision from '../photos/worldVision.png';
+import './style.css'
 
 const ProjectCards = () => {
     return (
-        <main>
-            <h2>My Recent Work</h2>
-            <div>
-                <div>
-                    <img src="" alt="" />
+        <main className="card-main">
+            <h2 className="project-title">My Recent Work</h2>
+            <div className="projects-cards">
+                <div className="card">
+                    <img src={worldVision} alt="World Vision" className="project-img"/>
                     <h3>Project 1</h3>
-                    <span>Ruby on Rails</span>
-                    <span>React</span>
-                    <span>Redux</span>
-                    <span>PostgreSQL</span>
-                    <span>CSS</span>
-                    <button>See Project</button>
+                    <div className="stack">
+                        <span>Ruby on Rails</span>
+                        <span>React</span>
+                        <span>Redux</span>
+                        <span>PostgreSQL</span>
+                        <span>CSS</span>
+                    </div>
+                    <button type="button" className="project-btn">See Project</button>
+                </div>
+                <div className="card">
+                    <img src={worldVision} alt="World Vision" className="project-img"/>
+                    <h3>Project 1</h3>
+                    <div className="stack">
+                        <span>Ruby on Rails</span>
+                        <span>React</span>
+                        <span>Redux</span>
+                        <span>PostgreSQL</span>
+                        <span>CSS</span>
+                    </div>
+                    <button type="button" className="project-btn">See Project</button>
+                </div>
+                <div className="card">
+                    <img src={worldVision} alt="World Vision" className="project-img"/>
+                    <h3>Project 1</h3>
+                    <div className="stack">
+                        <span>Ruby on Rails</span>
+                        <span>React</span>
+                        <span>Redux</span>
+                        <span>PostgreSQL</span>
+                        <span>CSS</span>
+                    </div>
+                    <button type="button" className="project-btn">See Project</button>
+                </div>
+                <div className="card">
+                    <img src={worldVision} alt="World Vision" className="project-img"/>
+                    <h3>Project 1</h3>
+                    <div className="stack">
+                        <span>Ruby on Rails</span>
+                        <span>React</span>
+                        <span>Redux</span>
+                        <span>PostgreSQL</span>
+                        <span>CSS</span>
+                    </div>
+                    <button type="button" className="project-btn">See Project</button>
+                </div>
+                <div className="card">
+                    <img src={worldVision} alt="World Vision" className="project-img"/>
+                    <h3>Project 1</h3>
+                    <div className="stack">
+                        <span>Ruby on Rails</span>
+                        <span>React</span>
+                        <span>Redux</span>
+                        <span>PostgreSQL</span>
+                        <span>CSS</span>
+                    </div>
+                    <button type="button" className="project-btn">See Project</button>
+                </div>
+                <div className="card">
+                    <img src={worldVision} alt="World Vision" className="project-img"/>
+                    <h3>Project 1</h3>
+                    <div className="stack">
+                        <span>Ruby on Rails</span>
+                        <span>React</span>
+                        <span>Redux</span>
+                        <span>PostgreSQL</span>
+                        <span>CSS</span>
+                    </div>
+                    <button type="button" className="project-btn">See Project</button>
                 </div>
             </div>        
         </main>
