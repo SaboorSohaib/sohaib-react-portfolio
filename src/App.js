d198a71176efce074a57c21bc75e87fe5125e661
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-05-20 07:38:08 +0430

    ADD ABOUT

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './components/home';
 import Project from './components/project';
+import About from './components/about';
 
 function App() {
   return (
     <div className="App">
       <Home />
       <Project />
+      <About />
     </div>
   );
 }
