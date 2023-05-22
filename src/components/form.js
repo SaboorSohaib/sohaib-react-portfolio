2c3fccb116e7ab9ec65b438c2de22bd13d2ea0ea
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-05-22 16:19:22 +0430

    ADD FORM

---
+++ src/components/form.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { 
+    faLinkedin,
+    faGithub,
+    faMedium,
+    faTwitter,
+    faAngellist,
+ } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './style.css'
+
+const Form = () => {
+    return (
+        <main className="main-form">
+            <h2 className="form-title">Contact Me</h2>
+            <div className="form-content"> 
+                <p>
+                  I'm always interested in hearing about new projects, so if you'd like to chat please get in touch.
+                </p>
+                <form className="form">
+                    <input type="text" id="name" className="input" name="name" placeholder="Your name.." />
+                    <input type="text" id="email" className="input" name="email" placeholder="Your email.." />
+                    <textarea id="subject" name="subject" className="input" placeholder="Write something.." ></textarea>
+                    <button type="submit" className="form-btn">Get in touch</button>
+                </form>
+            </div>
+            <hr  style={{
+                color: '#000000',
+                backgroundColor: '#000000',
+                height: '1px',
+                width: '100%',
+                borderColor : '#000000'
+            }}/>
+            <div className="social-icon">
+                    <ul>
+                        <li>
+                            <a href="https://www.linkedin.com/in/sohaib-ahmed-1b1b1b1b1/">
+                                <FontAwesomeIcon icon={faGithub} className="text-center" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/sohaib-ahmed-1b1b1b1b1/">
+                                <FontAwesomeIcon icon={faLinkedin} className="text-center" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/sohaib-ahmed-1b1b1b1b1/">
+                                <FontAwesomeIcon icon={faAngellist} className="text-center" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://medium.com/@sohaibahmedkhan">
+                                <FontAwesomeIcon icon={faMedium} className="text-center" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/SohaibAhmedKha1">
+                                <FontAwesomeIcon icon={faTwitter} className="text-center" />
+                            </a>
+                        </li>
+                        
+                    </ul>
+                </div>
+
+        </main>
+    );
+};
+
+export default Form;
