bf6bbffb644c70a2c222e6e9350d337e9745396f
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-05-23 21:45:32 +0430

    ADD PROJECT CARD

---
+++ src/components/projectCards.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ProjectCards = () => {
+    return (
+        <main>
+            <h2>My Recent Work</h2>
+            <div>
+                <div>
+                    <img src="" alt="" />
+                    <h3>Project 1</h3>
+                    <span>Ruby on Rails</span>
+                    <span>React</span>
+                    <span>Redux</span>
+                    <span>PostgreSQL</span>
+                    <span>CSS</span>
+                    <button>See Project</button>
+                </div>
+            </div>        
+        </main>
+    );
+};
+
+export default ProjectCards;
