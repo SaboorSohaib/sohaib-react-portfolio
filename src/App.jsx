5a85dc5f35780d9f39a710b5486766a3fea88977
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-05-25 22:57:33 +0430

    FIX SOME LINTER ERRORS

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Header from './components/header/Header.jsx';
-import Nav from './components/nav/Nav.jsx';
-import About from './components/about/About.jsx';
-import Experience from './components/experience/Experience.jsx';
-import Services from './components/services/Services.jsx';
-import Portfolio from './components/portfolio/Portfolio.jsx';
-import Testimonials from './components/testimonials/Testimonials.jsx';
-import Contact from './components/contact/Contact.jsx';
-import Footer from './components/footer/Footer.jsx';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Services from './components/services/Services';
+import Portfolio from './components/portfolio/Portfolio';
+import Testimonials from './components/testimonials/Testimonials';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 const App = () => (
   <>
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,17 +1,13 @@
-import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
-import {BsGithub} from 'react-icons/bs'
-import {BsMedium} from 'react-icons/bs'
-import './header.css'
+import React from 'react';
+import { BsLinkedin, BsGithub, BsMedium } from 'react-icons/bs';
+import './header.css';
 
-const HeaderSocials = () => {
-  return (
-    <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/abdul-saboor-sohaib/" target='_blank'><BsLinkedin/></a>
-        <a href="https://github.com/SaboorSohaib" target='_blank'><BsGithub /></a>
-        <a href="https://medium.com/@abdulsaboorsohaib" target='_blank'><BsMedium /></a>
-    </div>
-  )
-}
+const HeaderSocials = () => (
+  <div className="header__socials">
+    <a href="https://www.linkedin.com/in/abdul-saboor-sohaib/" target="_blank" rel="noreferrer"><BsLinkedin /></a>
+    <a href="https://github.com/SaboorSohaib" target="_blank" rel="noreferrer"><BsGithub /></a>
+    <a href="https://medium.com/@abdulsaboorsohaib" target="_blank" rel="noreferrer"><BsMedium /></a>
+  </div>
+);
 
-export default HeaderSocials
+export default HeaderSocials;
