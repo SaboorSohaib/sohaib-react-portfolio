539b5a221a672fbee8f888dc63fc012d7e7e2d0b
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-05-26 09:21:48 +0430

    ADD NAVBAR AND STYLING

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {AiOutlineHome} from 'react-icons/ai';
+import {AiOutlineUser} from 'react-icons/ai';
+import {AiOutlineFileText} from 'react-icons/ai';
+import {AiOutlineProject} from 'react-icons/ai';
+import {AiOutlineMail} from 'react-icons/ai';
 import './nav.css';
 
-const Nav = () => (
-  <div>Nav</div>
-);
+const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#');
+  return (
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><AiOutlineFileText /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineProject /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiOutlineMail /></a>
+    </nav>
+  )
+}
 
 export default Nav;
