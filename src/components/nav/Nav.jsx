f0465092cfde029b98f2fe66bc19c71a12eca705
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-05-28 12:36:11 +0430

    ADD TESTIMONIALS TO NAVBAR

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import {
   AiOutlineHome, AiOutlineUser, AiOutlineFileText, AiOutlineProject, AiOutlineMail,
 } from 'react-icons/ai';
+import { VscFeedback } from 'react-icons/vsc';
 import './nav.css';
 
 const Nav = () => {
@@ -12,6 +13,7 @@ const Nav = () => {
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''} aria-label="About" role="button"><AiOutlineUser /></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''} aria-label="Experience" role="button"><AiOutlineFileText /></a>
       <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''} aria-label="Portfolio" role="button"><AiOutlineProject /></a>
+      <a href="#testimonials" onClick={() => setActiveNav('#testimonials')} className={activeNav === '#testimonials' ? 'active' : ''} aria-label="Testimonials" role="button"><VscFeedback /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''} aria-label="Contact" role="button"><AiOutlineMail /></a>
     </nav>
   );
