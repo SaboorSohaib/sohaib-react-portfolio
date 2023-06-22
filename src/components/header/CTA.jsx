58998ebbe99192eeefd281633e48cd5fc3a0a7f9
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-06-22 15:40:22 +0430

    UPDATE RESUME

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import CV from '../../assets/CV.pdf';
+import Sohaib from '../../assets/Sohaib.pdf';
 
 const CTA = () => (
   <div className="cta">
-    <a href={CV} download className="btn">Download CV</a>
+    <a href={Sohaib} download className="btn">Download CV</a>
     <a href="#contact" className="btn btn-primary">Let&apos;s Talk</a>
   </div>
 );
