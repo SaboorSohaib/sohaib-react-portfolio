87d65561d139b3ca189c58a6ea3426ad0e16550c
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2024-08-04 16:24:28 +0430

    Fix error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import ReactGA from 'react-ga';
 import { ToastContainer } from 'react-toastify';
 import Header from './components/header/Header';
@@ -11,22 +11,21 @@ import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 
 const App = () => {
-  
   useEffect(() => {
     ReactGA.initialize('G-7D2J3T3VXM');
-    ReactGA.pageview(window.location.pathname + window.location.search)
-    },[]);
-  <>
-    <Header />
-    <Nav />
-    <About />
-    <Experience />
-    <Portfolio />
-    <Testimonials />
-    <Contact />
-    <Footer />
-    <ToastContainer position="top-right" />
-  </>
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+      <ToastContainer position="top-right" />
+    </>;
 };
 
 export default App;
