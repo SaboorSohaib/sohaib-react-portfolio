d07a05cb30627b2a4bf775b062ccf7410713af3f
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2024-08-21 09:29:33 +0430

    fix issue

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
+ReactDOM.render(<App />, document.querySelector('#root'));
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+// const rootElement = document.getElementById('root');
+// const root = ReactDOM.createRoot(rootElement);
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
